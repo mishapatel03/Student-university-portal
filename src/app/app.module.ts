ab1b4017f6182aaa134c47217ada5c3a31b01316
Simran <patel.simran.2104@gmail.com>
2020-10-19 19:26:37 +0530

    login module added, default app layout, other changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,5 @@
+import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 import { LayoutsModule } from './layouts/layouts.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
@@ -5,14 +7,18 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent
   ],
   imports: [
+    RouterModule,
     BrowserModule,
     AppRoutingModule,
+    FormsModule,
     BrowserAnimationsModule,
     LayoutsModule
   ],
